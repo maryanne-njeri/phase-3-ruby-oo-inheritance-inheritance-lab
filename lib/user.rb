620dde3b783cb13de6181a7d8a2e0a40605401c4
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-08-28 08:05:06 +0300

    Done

---
+++ lib/user.rb
@@ -0,0 +1,19 @@
+class User 
+
+    def first_name=(firstname)
+        @firstname = firstname
+    end
+
+    def first_name
+        @firstname
+    end
+
+    def last_name=(lastname)
+        @lastname = lastname
+    end
+
+    def last_name 
+        @lastname
+    end
+
+end
